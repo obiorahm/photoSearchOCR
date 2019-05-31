c61c759d6a28a09d226f743c05811f5e1182f4da
Mmachi Obiorah <obiorahm@gmail.com>
2019-05-31 12:52:18 -0500

    linearly align restaurant menu

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
@@ -86,23 +86,23 @@ public final class R {
         public static final int async = 0x7f080020;
         public static final int blocking = 0x7f080028;
         public static final int chronometer = 0x7f080035;
-        public static final int forever = 0x7f080063;
-        public static final int icon = 0x7f08006f;
-        public static final int icon_group = 0x7f080070;
-        public static final int info = 0x7f08007a;
-        public static final int italic = 0x7f080080;
-        public static final int line1 = 0x7f080084;
-        public static final int line3 = 0x7f080085;
-        public static final int normal = 0x7f0800a4;
-        public static final int notification_background = 0x7f0800a5;
-        public static final int notification_main_column = 0x7f0800a6;
-        public static final int notification_main_column_container = 0x7f0800a7;
-        public static final int right_icon = 0x7f0800da;
-        public static final int right_side = 0x7f0800db;
-        public static final int text = 0x7f080110;
-        public static final int text2 = 0x7f080111;
-        public static final int time = 0x7f080118;
-        public static final int title = 0x7f080119;
+        public static final int forever = 0x7f080064;
+        public static final int icon = 0x7f080070;
+        public static final int icon_group = 0x7f080071;
+        public static final int info = 0x7f08007b;
+        public static final int italic = 0x7f080081;
+        public static final int line1 = 0x7f080085;
+        public static final int line3 = 0x7f080086;
+        public static final int normal = 0x7f0800a5;
+        public static final int notification_background = 0x7f0800a6;
+        public static final int notification_main_column = 0x7f0800a7;
+        public static final int notification_main_column_container = 0x7f0800a8;
+        public static final int right_icon = 0x7f0800db;
+        public static final int right_side = 0x7f0800dc;
+        public static final int text = 0x7f080111;
+        public static final int text2 = 0x7f080112;
+        public static final int time = 0x7f080119;
+        public static final int title = 0x7f08011a;
     }
     public static final class integer {
         private integer() {}
